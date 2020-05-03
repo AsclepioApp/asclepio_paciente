7ff0bc60d485e55e409a2250fc88235490024d4c
Angel Torres <avto9711@gmail.com>
2020-05-02 20:58:21 -0400

    moving chat row files to component

--- lib/pages/chatLobby.dart
+++ lib/pages/chatLobby.dart
@@ -1,3 +1,4 @@
+import 'package:asclepio_paciente_flutter/components/ChatRow.dart';
 import 'package:asclepio_paciente_flutter/utilities/constants.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -54,8 +55,6 @@ class ChatLobbyState extends State<ChatLobby> {
                       )),
                     ),
                   )
-
-                  // Text("Chat Input Here")
                 ],
               ),
             ),
@@ -67,16 +66,6 @@ class ChatLobbyState extends State<ChatLobby> {
                 itemBuilder: (context,i){
                   return ChatRow();
               })  
-              
-              // Column(
-              //   children: <Widget>[
-              //     ChatRow(),
-              //     ChatRow(),
-              //     ChatRow(),
-              //     ChatRow(),
-              //     // Expanded(child: ,),
-              //   ],
-              // ),
             )
           ],
         ),
@@ -85,93 +74,3 @@ class ChatLobbyState extends State<ChatLobby> {
   }
 }
 
-class ChatRow extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    return Padding(
-      padding: EdgeInsets.all(5),
-      child: Container(
-        // color: Colors.blue,
-        child: IntrinsicHeight(
-          child: Row(
-            //  mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: <Widget>[
-              // Container(child: ,)
-              CircleAvatar(
-                maxRadius: 25,
-                backgroundImage: NetworkImage(
-                    "https://lh3.googleusercontent.com/-QWes7Nb78wY/WR9ZKwYZFXI/AAAAAAAATP4/RXRt-nZgEostobg8nYPAK1Bd3-_ZIESkwCEwYBhgL/w139-h140-p/IMG_0030.JPG"),
-              ),
-              // Image.asset(
-              //   "assets/images/profile_picture.png",
-              //   width: 60,
-              //   height: 60,
-              // ),
-              Expanded(
-                  child: Padding(
-                padding: EdgeInsets.all(8),
-                child: Container(
-                  // color: Colors.red,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: <Widget>[
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: <Widget>[
-                          Text(
-                            "Manuel Vladimir Oleaga Begaso",
-                            style: TextStyle(
-                                fontWeight: FontWeight.bold, fontSize: 15),
-                          ),
-                          Text("11:50 A.M"),
-                        ],
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: <Widget>[
-                          Text(
-                            "Hey doctor, operación de bimbím, muy corto...",
-                            overflow: TextOverflow.ellipsis,
-                            maxLines: 1,
-                            softWrap: false,
-                            style: TextStyle(
-                                color: Colors.grey[500],
-                                fontWeight: FontWeight.w400),
-                          ),
-                          Container(
-                            alignment: Alignment.center,
-                            padding: EdgeInsets.all(2),
-                            decoration: BoxDecoration(
-                              color: mainPurpleColor,
-                              borderRadius: BorderRadius.circular(10),
-                            ),
-                            constraints: BoxConstraints(
-                              minWidth: 20,
-                              minHeight: 20,
-                            ),
-                            child: Text(
-                              "5",
-                              style: TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.white),
-                            ),
-                          ),
-                        ],
-                      )
-                    ],
-                  ),
-                ),
-              )),
-              // Container(
-              //   // width: ,
-              //   color: Colors.black,
-              //   child: Text("Wawawawa"),
-              // )
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}
